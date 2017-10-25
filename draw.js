d3d33eb6d53a3f308065592ae6722a87cd44543a
Yvain Masson <ymasson@external.as24.com>
2017-10-25 12:19:20 +0200

    Ajout du bouton merge pour fusionner deux dessins au hasard

--- draw.js
+++ draw.js
@@ -30,6 +30,7 @@ document.onmouseup = function () {
 };
 var drawingArea = document.getElementById('drawing-area');
 var grid = document.getElementById('grid');
+var mergeArea = document.getElementById('mergeArea');
 //let gridArea = <HTMLCanvasElement> document.getElementById('grid');
 var drawingAreaWidth = drawingArea.width;
 var drawingAreaHeight = drawingArea.height;
@@ -59,6 +60,8 @@ function drawLine(startX, startY, endX, endY) {
 }
 var saveButton = document.getElementById('save');
 saveButton.addEventListener("click", saveCurrent);
+var mergeButton = document.getElementById('merge');
+mergeButton.addEventListener("click", mergeRandom);
 /*
 on commence en haut à gauche du caneva
 puis on garde ou était la dernière version sauvegardé
@@ -70,13 +73,46 @@ todo :
 */
 function saveCurrent(e) {
     var gridArea = document.createElement("canvas");
+    gridArea.height = 150;
+    gridArea.width = 150;
     gridArea.classList.add('drawing-area');
     gridArea.classList.add('grid-element');
     var ctxGrid = gridArea.getContext("2d");
     grid.appendChild(gridArea);
-    ctxGrid.scale(1, 0.5);
+    ctxGrid.scale(0.5, 0.5);
     ctxGrid.drawImage(drawingArea, 0, 0);
     ctx.clearRect(0, 0, canvasData.width, canvasData.height);
     ctx.beginPath();
     canvasData = ctx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
+}
+function mergeRandom(e) {
+    var n = grid.children.length;
+    var a = getRandomInt(0, n);
+    var b = getRandomInt(0, n);
+    while (a === b) {
+        b = getRandomInt(0, n);
+    }
+    var canvas1 = grid.children[a];
+    var canvas2 = grid.children[b];
+    var canvas1Ctx = canvas1.getContext("2d");
+    var canvas2Ctx = canvas2.getContext("2d");
+    var canvas1ImgData = canvas1Ctx.getImageData(0, 0, 150, 150);
+    var data1 = canvas1ImgData.data;
+    var canvas2ImgData = canvas2Ctx.getImageData(0, 0, 150, 150);
+    var data2 = canvas2ImgData.data;
+    var newImageData = new ImageData(150, 150);
+    for (var i = 0; i < canvas1ImgData.data.length; i++) {
+        newImageData.data[i] = data1[i] + data2[i];
+    }
+    var mergeRes = document.createElement("canvas");
+    mergeRes.height = 150;
+    mergeRes.width = 150;
+    mergeArea.appendChild(mergeRes);
+    var mergeCtx = mergeRes.getContext("2d");
+    mergeCtx.putImageData(newImageData, 0, 0);
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
--- draw.ts
+++ draw.ts
@@ -35,6 +35,7 @@ document.onmouseup = function() {
 
 let drawingArea = <HTMLCanvasElement> document.getElementById('drawing-area');
 let grid = <HTMLDivElement> document.getElementById('grid');
+let mergeArea = <HTMLDivElement> document.getElementById('mergeArea');
 //let gridArea = <HTMLCanvasElement> document.getElementById('grid');
 let drawingAreaWidth = drawingArea.width;
 let drawingAreaHeight = drawingArea.height;
@@ -70,6 +71,9 @@ let saveButton = <HTMLButtonElement> document.getElementById('save');
 
 saveButton.addEventListener("click", saveCurrent);
 
+let mergeButton = <HTMLButtonElement> document.getElementById('merge');
+mergeButton.addEventListener("click", mergeRandom);
+
 /*
 on commence en haut à gauche du caneva
 puis on garde ou était la dernière version sauvegardé 
@@ -82,15 +86,62 @@ todo :
 
 function saveCurrent(e: MouseEvent): void {
     let gridArea = document.createElement("canvas");
+    gridArea.height = 150;
+    gridArea.width = 150;
     gridArea.classList.add('drawing-area');
     gridArea.classList.add('grid-element');
     let ctxGrid = gridArea.getContext("2d");
     grid.appendChild(gridArea);
     
-    ctxGrid.scale(1, 0.5);
+    ctxGrid.scale(0.5, 0.5);
     ctxGrid.drawImage(drawingArea, 0, 0);
 
     ctx.clearRect(0, 0, canvasData.width, canvasData.height);
     ctx.beginPath();
     canvasData = ctx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
 }
+
+function mergeRandom(e: MouseEvent): void {
+    let n = grid.children.length;
+
+    let a = getRandomInt(0, n);
+    
+    let b = getRandomInt(0, n);
+    while(a === b)
+    {
+        b = getRandomInt(0, n);
+    }
+
+
+    let canvas1 = <HTMLCanvasElement>  grid.children[a];
+    let canvas2 = <HTMLCanvasElement>  grid.children[b];
+    let canvas1Ctx = canvas1.getContext("2d");
+    let canvas2Ctx = canvas2.getContext("2d");
+    
+    let canvas1ImgData = canvas1Ctx.getImageData(0,0, 150,150);
+    let data1 = canvas1ImgData.data;
+    let canvas2ImgData = canvas2Ctx.getImageData(0,0, 150,150);
+    let data2 = canvas2ImgData.data;
+
+    let newImageData = new ImageData(150, 150);
+
+    for(let i = 0; i < canvas1ImgData.data.length; i++ )
+    {
+        newImageData.data[i] = data1[i] + data2[i];
+    }
+
+    let mergeRes = document.createElement("canvas");    
+    mergeRes.height = 150;
+    mergeRes.width = 150;
+    mergeArea.appendChild(mergeRes);
+    let mergeCtx = mergeRes.getContext("2d");
+
+    mergeCtx.putImageData(newImageData, 0, 0);
+
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
