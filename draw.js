32b223bcc16118de51523de748c7bceee724944a
Yvain Masson <yvainmasson@fastmail.com>
2017-10-25 15:11:17 +0200

    Compiled draw.js

--- draw.js
+++ draw.js
@@ -1,90 +1,58 @@
-function draw(e) {
-    var X = e.pageX - this.offsetLeft;
-    var Y = e.pageY - this.offsetTop;
-    if (mouseDown) {
-        //drawPixel(X, Y, 0, 0, 255, 255);
-        if (lastX) {
-            drawLine(lastX, lastY, X, Y);
-            lastX = X;
-            lastY = Y;
-        }
-        else {
-            drawLine(X, Y, X, Y);
-            lastX = X;
-            lastY = Y;
-        }
-        //updateCanvas();
-    }
-}
-var mouseDown = 0;
+/* Handling mouse position */
 var lastX;
 var lastY;
+document.onmouseup = function () {
+    lastX = null;
+};
+/* Handling mouse button position */
+var mouseDown = 0;
 document.body.onmousedown = function () {
     ++mouseDown;
 };
 document.body.onmouseup = function () {
     --mouseDown;
 };
-document.onmouseup = function () {
-    lastX = null;
-};
+/* Drawing Area is a canvas where user can draw */
 var drawingArea = document.getElementById('drawing-area');
-var grid = document.getElementById('grid');
-var mergeArea = document.getElementById('mergeArea');
-//let gridArea = <HTMLCanvasElement> document.getElementById('grid');
 var drawingAreaWidth = drawingArea.width;
 var drawingAreaHeight = drawingArea.height;
-var ctx = drawingArea.getContext("2d");
-//let ctxGrid = gridArea.getContext("2d");
-var canvasData = ctx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
+var drawingAreaCtx = drawingArea.getContext("2d");
+var drawingAreaImageData = drawingAreaCtx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
 drawingArea.addEventListener("mousemove", draw);
-/*
-function updateCanvas() {
-    ctx.putImageData(canvasData, 0, 0);
-}
-
-function drawPixel (x, y, r, g, b, a) {
-    var index = (x + y * drawingAreaWidth) * 4;
-
-    canvasData.data[index + 0] = r;
-    canvasData.data[index + 1] = g;
-    canvasData.data[index + 2] = b;
-    canvasData.data[index + 3] = a;
-}
-*/
-function drawLine(startX, startY, endX, endY) {
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
-}
+/* Grid is where we display saved drawings */
+var grid = document.getElementById('grid');
+/* Merge Area is where we display the result of merged drawings */
+var mergeArea = document.getElementById('merge-area');
+/* Save button stores current drawing from drawingArea to grid, and flush drawing area */
 var saveButton = document.getElementById('save');
 saveButton.addEventListener("click", saveCurrent);
+/* Merge button takes two random existing drawing, merge them into one and display it on mergeArea */
 var mergeButton = document.getElementById('merge');
 mergeButton.addEventListener("click", mergeRandom);
-/*
-on commence en haut à gauche du caneva
-puis on garde ou était la dernière version sauvegardé
-
-todo :
-- trait continu
-- scale down
-- mettre à la suite
-*/
+/**
+ * Function that stores current drawin in grid and flush drawing area
+ * @param e {MouseEvent} click
+ */
 function saveCurrent(e) {
-    var gridArea = document.createElement("canvas");
-    gridArea.height = 150;
-    gridArea.width = 150;
-    gridArea.classList.add('drawing-area');
-    gridArea.classList.add('grid-element');
-    var ctxGrid = gridArea.getContext("2d");
-    grid.appendChild(gridArea);
-    ctxGrid.scale(0.5, 0.5);
-    ctxGrid.drawImage(drawingArea, 0, 0);
-    ctx.clearRect(0, 0, canvasData.width, canvasData.height);
-    ctx.beginPath();
-    canvasData = ctx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
+    var drawing = document.createElement("canvas");
+    drawing.height = 150;
+    drawing.width = 150;
+    drawing.classList.add('drawing-area');
+    drawing.classList.add('grid-element');
+    var drawingCtx = drawing.getContext("2d");
+    grid.appendChild(drawing);
+    drawingCtx.scale(0.5, 0.5);
+    drawingCtx.drawImage(drawingArea, 0, 0);
+    drawingAreaCtx.clearRect(0, 0, drawingAreaImageData.width, drawingAreaImageData.height);
+    drawingAreaCtx.beginPath();
+    drawingAreaImageData = drawingAreaCtx.getImageData(0, 0, drawingAreaWidth, drawingAreaHeight);
 }
+/**
+ * Randomly takes two drawing in grid
+ * Merge them to a new image
+ * Display new image in merge area
+ * @param {MouseEvent} e
+ */
 function mergeRandom(e) {
     var n = grid.children.length;
     var a = getRandomInt(0, n);
@@ -108,11 +76,48 @@ function mergeRandom(e) {
     mergeRes.height = 150;
     mergeRes.width = 150;
     mergeArea.appendChild(mergeRes);
-    var mergeCtx = mergeRes.getContext("2d");
-    mergeCtx.putImageData(newImageData, 0, 0);
+    var mergeResCtx = mergeRes.getContext("2d");
+    mergeResCtx.putImageData(newImageData, 0, 0);
 }
+/**
+ * Draw a line between current and last mouse position
+ * @param {MouseEvent} e
+ */
+function draw(e) {
+    var X = e.pageX - this.offsetLeft;
+    var Y = e.pageY - this.offsetTop;
+    if (mouseDown) {
+        if (lastX) {
+            drawLine(lastX, lastY, X, Y);
+        }
+        else {
+            drawLine(X, Y, X, Y);
+        }
+        lastX = X;
+        lastY = Y;
+    }
+}
+/**
+ * Draw a line on canvas drawingArea
+ * @param {number} startX - x coordinates of the starting point
+ * @param {number} startY - y coordinates of the starting point
+ * @param {number} endX - x coordinates of the ending point
+ * @param {number} endY - y coordinates of the ending point
+ */
+function drawLine(startX, startY, endX, endY) {
+    drawingAreaCtx.beginPath();
+    drawingAreaCtx.moveTo(startX, startY);
+    drawingAreaCtx.lineTo(endX, endY);
+    drawingAreaCtx.stroke();
+}
+/**
+ * From MDN get a random integer between min and max
+ * The maximum is exclusive and the minimum is inclusive
+ * @param {int} min - lower bound
+ * @param {int} max - upper bound
+ */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min;
 }
